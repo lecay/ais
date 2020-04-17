87f5a2c4252af987a0943955b75c5ed2501efacb
lecay <lecay@users.noreply.github.com>
2020-04-17 22:00:14 +0800

    调整为2行的形式

--- dailyais.py
+++ dailyais.py
@@ -20,25 +20,27 @@ print(intime)
 if os.path.exists(filepath+intime+'.json'):
     with open(filepath+intime+'.json', 'rb') as f:
         data = json.load(f)
-allrow = len(data.keys())
+allrow = len(data.keys())*2+1
 
-ysize = allrow*0.21+1.4    #图片长度
+ysize = allrow*0.2+1.4    #图片长度
 fig = plt.figure(figsize=(8,ysize),dpi=130)
-ax1 = fig.add_axes([0.05,0.6/(ysize),0.9,allrow*0.21/(ysize)])
+ax1 = fig.add_axes([0.05,0.6/(ysize),0.9,allrow*0.2/(ysize)])
 font = fm.FontProperties(fname=r"C:/Windows/Fonts/msyh.ttc")
 
-fcx = 1.25      #机场四字码与中文名分割线x坐标
-cnx = 1.6        #机场中文名与跑道号分割线x坐标
-stawx = 2.5    #机场与天气区分割线x坐标
-allx = 10.9    #x轴总长度
+#fcx = 1.25      #机场四字码与中文名分割线x坐标
+cnx = 1.4        #机场中文名与跑道号分割线x坐标
+anx = 1.9        #跑道号与通告号分割线x坐标
+stawx = 2.8    #通告号与通告展示区分割线x坐标
+allx = 11.2    #x轴总长度
 
 ax1.plot([cnx,cnx],[allrow,0],color='lightgrey',linewidth=0.5)    #机场中文名与跑道号分割线
-ax1.plot([stawx,stawx],[allrow,0],color='dimgray',linewidth=1)    #机场与天气分割线
+ax1.plot([anx,anx],[allrow,0],color='lightgrey',linewidth=0.5)    #跑道号与通告号分割线
+ax1.plot([stawx,stawx],[allrow,0],color='dimgray',linewidth=1)    #通告号与通告展示区分割线
 # for x in range(1,24):
 #     ax1.plot([stawx+x*0.35,stawx+x*0.35],[0,allrow],color='lightgrey',linewidth=0.5)    #画纵向网格线
 for x in range(1,allrow):
-    ax1.plot([0,stawx],[x,x],color='dimgray',linewidth=1)    #机场分割线
-    ax1.plot([stawx,allx],[x,x],color='darkslategrey',linewidth=0.5)    #画横向网格线
+    ax1.plot([0,allx],[x*2,x*2],color='dimgray',linewidth=1)    #机场分割线
+    ax1.plot([stawx,allx],[x*2-1,x*2-1],color='lightgrey',linewidth=0.5)    #画横向网格线
 
 # h小时，wy填充下边界y坐标，wid填色宽度，height填色高度，color填色颜色，rim是否画右边界，dheight填色下边高度，isstr是否填写字符，fstr字符内容
 def drawwx(timestr,wy,wid,height,color,dheight=0,isstr=False,fstr='',alpha=1):
@@ -54,10 +56,11 @@ x = 0
 for sta,detail in data.items():
     x = x+1
     wy = allrow-x
-    stainfo = sta.split(); starwy = stainfo[0:-1]
-    ax1.text(0.1,wy+0.4,' '.join(starwy),ha='left', va='center',fontproperties=font, size=8)    #机场四字码及跑道号
-    ax1.text(2.05,wy+0.4,stainfo[-1],ha='center', va='center',fontproperties=font, size=8, color='dimgrey')    #通告号
-    ax1.text(2.6,wy+0.4,detail['notamContent'],ha='left', va='center',fontproperties=font, size=8)    #通告内容
+    stainfo = sta.split(); staid = stainfo[0]; rwy = ''.join(stainfo[1:-1])
+    ax1.text(0.1,wy+0.4,staid,ha='left', va='center',fontproperties=font, size=8)    #机场四字码
+    ax1.text((anx+cnx)/2,wy+0.4,rwy[3:],ha='center', va='center',fontproperties=font, size=8)    #跑道号
+    ax1.text((stawx+anx)/2,wy+0.4,stainfo[-1],ha='center', va='center',fontproperties=font, size=8, color='dimgrey')    #通告号
+    ax1.text(stawx+0.1,wy+0.4,detail['notamContent'],ha='left', va='center',fontproperties=font, size=8)    #通告内容
     if detail['style'] == 'EST':
         fillcolor = 'orange'
     else:
